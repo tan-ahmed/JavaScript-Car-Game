194f2069539938fbe62be00712294d4b3e84e2fb
tan-ahmed <tanner_ahmed@outlook.com>
2019-02-25 21:44:29 +0000

    0.4.1 check car collision

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const speedDash = document.querySelector('.speedDash');
-const scoreDash = document.querySelector('.scoreDash');
-const lifeDash = document.querySelector('.lifeDash');
-const container = document.getElementById('container');
-const btnStart = document.querySelector('.btnStart');
-btnStart.addEventListener('click', startGame);
-document.addEventListener('keydown', pressKeyOn);
-document.addEventListener('keyup', pressKeyOff);
+const speedDash = document.querySelector(".speedDash");
+const scoreDash = document.querySelector(".scoreDash");
+const lifeDash = document.querySelector(".lifeDash");
+const container = document.getElementById("container");
+const btnStart = document.querySelector(".btnStart");
+btnStart.addEventListener("click", startGame);
+document.addEventListener("keydown", pressKeyOn);
+document.addEventListener("keyup", pressKeyOff);
 //Game Variables
 let animationGame;
 let gamePlay = false;
@@ -15,13 +15,13 @@ let keys = {
   ArrowDown: false,
   ArrowLeft: false,
   ArrowRight: false
-}
+};
 
 function startGame() {
   // console.log(gamePlay);
-  btnStart.style.display = 'none';
-  var div = document.createElement('div');
-  div.setAttribute('class', 'playerCar');
+  btnStart.style.display = "none";
+  var div = document.createElement("div");
+  div.setAttribute("class", "playerCar");
   div.x = 250;
   div.y = 500;
   container.appendChild(div);
@@ -35,45 +35,49 @@ function startGame() {
     carstoPass: 10,
     score: 0,
     roadwidth: 250
-  }
+  };
   startBoard();
   setupBadGuys(10);
 }
 
-function setupBadGuys(num){
-  for(let x = 0; x<num; x++){
-    let temp = 'badGuy'+(x+1);
-    let div = document.createElement('div');
-    div.innerHTML = (x+1);
-    div.setAttribute('class', 'baddy');
-    div.setAttribute('id', temp);
+function setupBadGuys(num) {
+  for (let x = 0; x < num; x++) {
+    let temp = "badGuy" + (x + 1);
+    let div = document.createElement("div");
+    div.innerHTML = x + 1;
+    div.setAttribute("class", "baddy");
+    div.setAttribute("id", temp);
     makeBad(div);
     container.appendChild(div);
   }
 }
 
-function randomColor(){
-  function c(){
-    let hex = Math.floor(Math.random()*256).toString(16);
-    return('0'+String(hex)).substr(-2);
+function randomColor() {
+  function c() {
+    let hex = Math.floor(Math.random() * 256).toString(16);
+    return ("0" + String(hex)).substr(-2);
   }
-  return '#'+c()+c()+c();
+  return "#" + c() + c() + c();
 }
 
-function makeBad(e){
-  let tempRoad = document.querySelector('.road');
-  e.style.left = tempRoad.offsetLeft + Math.ceil(Math.random()*tempRoad.offsetWidth)-30+'px';
-  e.style.top = Math.ceil(Math.random() * -400) + 'px';
-  e.speed = Math.ceil(Math.random() * 17) +2;
+function makeBad(e) {
+  let tempRoad = document.querySelector(".road");
+  e.style.left =
+    tempRoad.offsetLeft +
+    Math.ceil(Math.random() * tempRoad.offsetWidth) -
+    30 +
+    "px";
+  e.style.top = Math.ceil(Math.random() * -400) + "px";
+  e.speed = Math.ceil(Math.random() * 17) + 2;
   e.style.backgroundColor = randomColor();
 }
 
 function startBoard() {
   for (let x = 0; x < 13; x++) {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'road');
-    div.style.top = (x * 50) + 'px';
-    div.style.width = player.roadwidth + 'px';
+    let div = document.createElement("div");
+    div.setAttribute("class", "road");
+    div.style.top = x * 50 + "px";
+    div.style.width = player.roadwidth + "px";
     container.appendChild(div);
   }
 }
@@ -91,14 +95,14 @@ function pressKeyOff(event) {
 }
 
 function updateDash() {
-  //console.log(player);  
+  //console.log(player);
   scoreDash.innerHTML = player.score;
   lifeDash.innerHTML = player.lives;
   speedDash.innerHTML = Math.round(player.speed * 13);
 }
 
 function moveRoad() {
-  let tempRoad = document.querySelectorAll('.road');
+  let tempRoad = document.querySelectorAll(".road");
   // console.log(tempRoad);
   let previousRoad = tempRoad[0].offsetLeft;
   let previousWidth = tempRoad[0].offsetWidth;
@@ -108,35 +112,44 @@ function moveRoad() {
     if (num > 600) {
       num = num - 650;
       let mover = previousRoad + (Math.floor(Math.random() * 6) - 3);
-      let roadWidth = (Math.floor(Math.random() * 11) - 5) + previousWidth;
+      let roadWidth = Math.floor(Math.random() * 11) - 5 + previousWidth;
       if (roadWidth < 200) roadWidth = 200;
       if (roadWidth > 400) roadWidth = 400;
       if (mover < 100) mover = 100;
       if (mover > 600) mover = 600;
-      tempRoad[x].style.left = mover + 'px';
-      tempRoad[x].style.width = roadWidth + 'px';
+      tempRoad[x].style.left = mover + "px";
+      tempRoad[x].style.width = roadWidth + "px";
       previousRoad = tempRoad[x].offsetLeft;
       previousWidth = tempRoad[x].width;
     }
-    tempRoad[x].style.top = num + 'px';
+    tempRoad[x].style.top = num + "px";
   }
   //returns an object with the values from the top
-  return {'width': previousWidth, 'left': previousRoad};
+  return { width: previousWidth, left: previousRoad };
+}
+
+function isCollide(a, b){
+  let aRect = a.getBoundingClientRect();
+  let bRect = b.getBoundingClientRect();
+  // console.log(aRect);
+  return !(
+    (aRect.bottom < bRect.top)  || (aRect.top > bRect.bottom) || (aRect.right < bRect.left) || (aRect.left > bRect.right)
+  )
 
 }
 
-function moveBadGuys(){
-  let tempBaddy = document.querySelectorAll('baddy');
-  for (let i = 0; i<tempBaddy.length; i++){
+function moveBadGuys() {
+  let tempBaddy = document.querySelectorAll(".baddy");
+  for (let i = 0; i < tempBaddy.length; i++) {
     let y = tempBaddy[i].offsetTop + player.speed - tempBaddy[i].speed;
-    if(y>2000 || y < -2000){
+    if (y > 2000 || y < -2000) {
       //reset car
       makeBad(tempBaddy[i]);
     } else {
-      tempBaddy[i].style.top = y + 'px';
+      tempBaddy[i].style.top = y + "px";
+      let hitCar = isCollide(tempBaddy[i], player.ele);
+      console.log(hitCar);
     }
-
-
   }
 }
 
@@ -147,34 +160,39 @@ function playGame() {
     let roadPara = moveRoad();
     moveBadGuys();
     // console.log(roadPara);
-    
 
     if (keys.ArrowUp) {
       if (player.ele.y > 400) player.ele.y -= 1;
-      player.speed = player.speed < 20 ? (player.speed + 0.05) : 20;
+      player.speed = player.speed < 20 ? player.speed + 0.05 : 20;
     }
     if (keys.ArrowDown) {
-      if (player.ele.y < 500) { player.ele.y += 1; }
-      player.speed = player.speed > 0 ? (player.speed - 0.2) : 0;
+      if (player.ele.y < 500) {
+        player.ele.y += 1;
+      }
+      player.speed = player.speed > 0 ? player.speed - 0.2 : 0;
     }
     if (keys.ArrowRight) {
-      player.ele.x += (player.speed / 4);
+      player.ele.x += player.speed / 4;
     }
     if (keys.ArrowLeft) {
-      player.ele.x -= (player.speed / 4);
+      player.ele.x -= player.speed / 4;
     }
     //check if on road
-    if ((player.ele.x + 40) < roadPara.left || (player.ele.x > (roadPara.left + roadPara.width))) {
-      
-      if (player.ele.y < 500) { player.ele.y += +1; }
+    if (
+      player.ele.x + 40 < roadPara.left ||
+      player.ele.x > roadPara.left + roadPara.width
+    ) {
+      if (player.ele.y < 500) {
+        player.ele.y += +1;
+      }
 
-      player.speed = player.speed > 0 ? (player.speed - 0.2): 1;
-      console.log('off road');
+      player.speed = player.speed > 0 ? player.speed - 0.2 : 1;
+      // console.log("off road");
     }
 
     ///move car
-    player.ele.style.top = player.ele.y + 'px';
-    player.ele.style.left = player.ele.x + 'px';
+    player.ele.style.top = player.ele.y + "px";
+    player.ele.style.left = player.ele.x + "px";
   }
   animationGame = requestAnimationFrame(playGame);
 }
